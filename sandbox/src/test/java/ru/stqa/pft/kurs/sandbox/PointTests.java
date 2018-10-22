211d0204eeeeac80d1dd8326459cfd8f1f9d72fe
Ewelina Rybak <wcia@radiowy.net>
2018-10-22 21:42:49 +0200

    PointTest

---
+++ sandbox/src/test/java/ru/stqa/pft/kurs/sandbox/PointTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.kurs.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testArea() {
+
+    Point p1 = new Point(7.9, 1.9);
+
+    Point p2 = new Point(1.7, 3.1);
+    
+  }
+
+  }
+
