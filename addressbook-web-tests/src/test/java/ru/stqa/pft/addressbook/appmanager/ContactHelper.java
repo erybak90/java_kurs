e1666cab2e46af13e313fd212bf223d7d6292e4e
Ewelina Rybak <wcia@radiowy.net>
2018-12-06 21:23:20 +0100

    correct no 6

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -36,19 +36,20 @@ public class ContactHelper extends HelperBase {
   }
 
   public void initContactModification() {
-    click(By.name("Edit"));
+    click(By.xpath("//div/div[4]/form[2]/table/tbody/tr[2]/td[1]/input"));
+    click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
   }
 
   public void submitContactModification() {
-    click(By.name("Update"));
+    click(By.xpath("//div[@id='content']/form[1]/input[22]"));
   }
 
   public void selectContactDeleted() {
-    click(By.name("Selector[]"));
+    click(By.xpath("//div/div[4]/form[2]/table/tbody/tr[2]/td[1]/input"));
   }
 
   public void clickDelete() {
-    click(By.name("Delete"));
+    click(By.xpath("//div[@id='content']/form[2]/div[2]/input"));
   }
 
   public void submitContactDelete() {
