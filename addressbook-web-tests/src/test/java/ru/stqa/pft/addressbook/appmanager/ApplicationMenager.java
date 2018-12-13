be2a80a029e58b05ce4343f0eaa86aefefaa798c
Ewelina Rybak <wcia@radiowy.net>
2018-12-13 19:03:36 +0100

    No 7

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationMenager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationMenager.java
@@ -1,26 +1,40 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationMenager {
-  FirefoxDriver wd;
+  private final String browser;
+  private WebDriver wd;
 
-  private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private ContactHelper contactHelper;
   private GroupHelper groupHelper;
 
+  public ApplicationMenager(String browser) {
+    this.browser = browser;
+  }
+
   public void init() {
-    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    if (browser.equals(BrowserType.FIREFOX)){
+      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    } else if (browser.equals(BrowserType.CHROME)){
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)){
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
     contactHelper = new ContactHelper(wd);
     navigationHelper = new NavigationHelper(wd);
-    sessionHelper = new SessionHelper (wd);
+    SessionHelper sessionHelper = new SessionHelper(wd);
     sessionHelper.login("admin", "secret");
   }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationMenager;
 
 public class TestBase {
 
-  protected final ApplicationMenager app = new ApplicationMenager();
+  protected final ApplicationMenager app = new ApplicationMenager(BrowserType.CHROME);
 
   @BeforeMethod
   public void setUp() throws Exception {
