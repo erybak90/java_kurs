2ce3a74eaa058880c49048b126f99dcd6889dc7b
Ewelina Rybak <wcia@radiowy.net>
2018-12-28 17:06:13 +0100

    Revert "4.3contacts"
    This reverts commit 76e7c09cfbbe0899747f0655a45dfc213b741f45.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,7 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -10,13 +9,10 @@ public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreation() {
-        int before = app.getContactHelper().getContactCount();
         app.getContactHelper().gotoAddContact();
         app.getContactHelper().createContact(new ContactData("lastname1", "mobilephone1",
                 "email1", "firstname1", "test1"), true);
         app.getNavigationHelper().retourntoContactsPage();
-        int after = app.getContactHelper().getContactCount();
-        Assert.assertEquals(after, before +1);
     }
 
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -8,19 +7,15 @@ public class ContactModificationTests extends TestBase {
 
   @Test
   public void testContactModification() {
-    int before = app.getContactHelper().getContactCount();
     if (! app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData("lastname1", "mobilephone1",
               "email1", "firstname1", "test1"), true);
 
     }
-    app.getNavigationHelper().retourntoContactsPage();
    app.getContactHelper().initContactModification();
    app.getContactHelper().fillContactForm(new ContactData("lastname1", "mobilephone1",
            "email1", "firstname1", null), false);
    app.getContactHelper().submitContactModification();
    app.getNavigationHelper().retourntoContactsPage();
-    int after = app.getContactHelper().getContactCount();
-    Assert.assertEquals(after, before);
   }
 }
