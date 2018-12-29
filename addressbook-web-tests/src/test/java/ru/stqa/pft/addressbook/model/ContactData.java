cfa45c7fa3b6ac96d97e74023886e232c889a42c
Ewelina Rybak <wcia@radiowy.net>
2018-12-29 23:12:30 +0100

    4.6 contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,10 +1,14 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.util.Objects;
+
 public class ContactData {
   private final String lastname;
   private final String mobilephone;
   private final String email;
   private final String firstname;
+
+
   private String group;
 
   public ContactData(String lastname, String mobilephone, String email, String firstname, String group) {
@@ -33,5 +37,27 @@ public class ContactData {
 
   public String getGroup() {
     return group;
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "lastname='" + lastname + '\'' +
+            ", firstname='" + firstname + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ContactData that = (ContactData) o;
+    return Objects.equals(lastname, that.lastname) &&
+            Objects.equals(firstname, that.firstname);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(lastname, firstname);
   }
 }
