2346b1b450df09de811f5120a4b48d3902ab1b31
Ewelina Rybak <wcia@radiowy.net>
2018-12-29 15:32:33 +0100

    Revert "4.10"
    This reverts commit d78af07562480b13af67e1d2a531af7928464ca1.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -7,10 +7,8 @@ public class GroupData {
   private final String footer;
 
 
-
-
   public GroupData(String name, String header, String footer) {
-    this.id = Integer.MAX_VALUE;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -57,12 +55,15 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
+    if (id != groupData.id) return false;
     return name != null ? name.equals(groupData.name) : groupData.name == null;
   }
+
   @Override
   public int hashCode() {
-    return name != null ? name.hashCode() : 0;
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
   }
-
 
 }
