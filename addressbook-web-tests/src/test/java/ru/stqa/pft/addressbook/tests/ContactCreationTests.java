5bfbeaa75d78af05288015022f8b3ef902efc912
Ewelina Rybak <wcia@radiowy.net>
2019-01-12 23:54:34 +0100

    no 15

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
@@ -9,6 +10,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -18,16 +20,18 @@ public class ContactCreationTests extends TestBase {
 
     @DataProvider
     public Iterator<Object[]> validContacts() throws IOException {
-        List<Object[]> list = new ArrayList<Object[]>();
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
          String line = reader.readLine();
          while (line !=null){
-             String[] split = line.split(";");
-             list.add(new Object[] {new ContactData().withFirstname(split[0]).
-                     withLastname(split[1]).withMobilephone(split[2])});
+          xml += line;
              line = reader.readLine();
          }
-        return list.iterator();
+         XStream xstream = new XStream();
+         xstream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+
 
         //list.add(new Object[]{new ContactData().withLastname("lastname 1").withFirstname("firstname 1")
                 //.withMobilephone("mobilephone 1")});
