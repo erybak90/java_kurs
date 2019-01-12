3a882e0b2e71997f43839e75ce8d079afdfb8351
Ewelina Rybak <wcia@radiowy.net>
2019-01-12 21:58:27 +0100

    6.3 contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,9 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import org.testng.xml.dom.ParentSetter;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.io.File;
@@ -9,25 +13,41 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ContactDataGenerator { 
-    
-    public static void main (String[] args) throws IOException {
- int count = Integer.parseInt(args[0]);
- File  file = new File(args[1]);
+public class ContactDataGenerator {
 
-        List<ContactData> contacts = generateContacts(count);
-        save(contacts, file);
- 
-        
+    @Parameter (names = "-c", description = "Contact count")
+    public int count;
+
+    @Parameter (names = "-f", description = "Target file")
+    public String file;
+
+
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try{
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+
+        generator.run();
     }
 
-    private static void save(List<ContactData> contacts, File file) throws IOException {
+    private void run() throws IOException {
+        List<ContactData> contacts = generateContacts(count);
+        save(contacts, new File(file));
+
+    }
+
+    private void save(List<ContactData> contacts, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
-      Writer writer =  new FileWriter(file);
-      for (ContactData contact : contacts) {
-          writer.write(String.format("%s;%s;%s\n", contact.getFirstname(), contact.getLastname(), contact.getMobilephone()));
-      }
-     writer.close();
+        Writer writer = new FileWriter(file);
+        for (ContactData contact : contacts) {
+            writer.write(String.format("%s;%s;%s\n", contact.getFirstname(), contact.getLastname(), contact.getMobilephone()));
+        }
+        writer.close();
     }
 
     private static List<ContactData> generateContacts(int count) {
