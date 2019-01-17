af750ce6be16091bfc3a3f45c3ab3f6490068b59
Ewelina Rybak <wcia@radiowy.net>
2019-01-17 21:27:58 +0100

    8.5

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationMenager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationMenager.java
@@ -12,11 +12,13 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.MatchResult;
 
 public class ApplicationMenager {
   private final String browser;
   private final Properties properties;
   private WebDriver wd;
+  private RegistrationHelper registrationHelper;
 
 
   public ApplicationMenager(String browser) {
@@ -28,28 +30,42 @@ public class ApplicationMenager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-
-    if (browser.equals(BrowserType.FIREFOX)){
-      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    } else if (browser.equals(BrowserType.CHROME)){
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)){
-      wd = new InternetExplorerDriver();
-    }
-
-}
+  }
 
 
   public void stop() {
-    wd.quit();
+    if (wd != null) {
+      wd.quit();
+    }
   }
 
-public HttpSession newSession(){
+  public HttpSession newSession() {
     return new HttpSession(this);
-}
+  }
 
   public String getProperty(String key) {
     return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+
+
+  public WebDriver getDriver() {
+    if (wd == null){
+      if (browser.equals(BrowserType.FIREFOX)){
+        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+      } else if (browser.equals(BrowserType.CHROME)){
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)){
+        wd = new InternetExplorerDriver();
+      }
+    }
+    return wd;
   }
 }
---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+
+public class RegistrationHelper {
+    private final ApplicationMenager app;
+    private WebDriver wd;
+
+    public RegistrationHelper(ApplicationMenager app) {
+        this.app = app;
+        wd = app.getDriver();
+    }
+
+    public void start(String username, String email) {
+        wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+
+    }
+}
